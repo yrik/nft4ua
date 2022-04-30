0eda253e2de11d1d3406fbc414c05b0b58be7505
yrik <iurii.kriachko@gmail.com>
2022-04-30 18:48:02 +0800

    1 file modified

--- src/Art.js
+++ src/Art.js
@@ -4,9 +4,36 @@ import Images from './art/art.json';
 const Art = (props) => {
   return (
     <div className="prose text-center m-auto mt-5 px-5">
-      {Images.map((img) => (
-        <img src={`${process.env.PUBLIC_URL}/art-dest/${img}`} />
-      ))}
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto">
+        {Images.map((img) => (
+          <div className="w-full rounded mx-auto">
+            <img
+              className="rounded"
+              style={{
+                minWidth: '300px',
+                width: '300px',
+                height: '300px',
+                display: 'inline-block',
+              }}
+              src={`${process.env.PUBLIC_URL}/art-dest/${img}`}
+            />
+            {/*
+            <div
+              className="mx-auto"
+              style={{
+                width: '300px',
+                marginTop: '-65px',
+                backgroundColor: '#808080',
+                color: 'white',
+                borderRadius: '0px 5px',
+              }}
+            >
+              {img.replace('.jpeg', '')}
+            </div>
+            */}
+          </div>
+        ))}
+      </div>
 
       <a href="/" className="mt-10 btn btn-outline btn-secondary">
         Back to Main
